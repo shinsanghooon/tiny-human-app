6612ed940a75e06dad6f8b2700642e8673de31e6
sanghoon <shinsanghooon@gmail.com>
2023-11-29 15:56:13 +0900

    feature: 다이어리 조회 시 더미 데이터 제거

--- lib/diary/view/diary_screen.dart
+++ lib/diary/view/diary_screen.dart
@@ -1,76 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stacked_card_carousel/stacked_card_carousel.dart';
 import 'package:tiny_human_app/baby/view/baby_register_screen.dart';
+import 'package:tiny_human_app/common/dio/dio.dart';
 import 'package:tiny_human_app/diary/component/diary_card.dart';
 import 'package:tiny_human_app/diary/model/diary_response_model.dart';
+import 'package:tiny_human_app/diary/provider/diary_pagination_provider.dart';
+import 'package:tiny_human_app/diary/view/diary_detail_screen.dart';
 
 import '../../common/constant/colors.dart';
 import '../../common/constant/data.dart';
 import '../../common/layout/default_layout.dart';
+import '../../common/model/cursor_pagination_model.dart';
+import '../component/diary_card_2.dart';
 import 'diary_register_screen.dart';
 
-class DiaryScreen extends ConsumerWidget {
+class DiaryScreen extends ConsumerStatefulWidget {
   const DiaryScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<DiaryScreen> createState() => _DiaryScreenState();
+}
+
+class _DiaryScreenState extends ConsumerState<DiaryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    print("diary screen");
+    final diaryPagination = ref.watch(diaryPaginationProvider);
+
+    if (diaryPagination is CursorPaginationLoading) {
+      return const Center(
+          child: CircularProgressIndicator(
+        color: PRIMARY_COLOR,
+      ));
+    }
+
+    if (diaryPagination is CursorPaginationError) {
+      return Center(
+        child: Text("에러가 있습니다. ${diaryPagination.message}"),
+      );
+    }
+
+    final cp = diaryPagination as CursorPagination;
+    final data = cp.body;
+
     return DefaultLayout(
-      child: CustomScrollView(
+      child: RefreshIndicator(
+        edgeOffset: 120.0, // TODO: AppBar 높이 알아내서 반영하기
+        color: PRIMARY_COLOR,
+
+        onRefresh: () async {
+          ref.read(diaryPaginationProvider.notifier).paginate(
+            forceRefetch: true
+          );
+        },
+        child: CustomScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
           slivers: [
-        SliverAppBar(
-          backgroundColor: Colors.transparent,
-          title: const Text(
-            "DIARY",
-            style: TextStyle(
-              color: Colors.deepOrange,
-              fontWeight: FontWeight.w800,
+            SliverAppBar(
+              backgroundColor: Colors.transparent,
+              title: const Text(
+                "DIARY",
+                style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              leading: IconButton(
+                icon: const Icon(Icons.home_outlined, color: PRIMARY_COLOR),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.add, color: PRIMARY_COLOR),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => DiaryRegisterScreen(),
+                      ),
+                    );
+                  },
+                )
+              ],
             ),
-          ),
-          leading: IconButton(
-            icon: const Icon(Icons.home_outlined, color: PRIMARY_COLOR),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          actions: [
-            IconButton(
-                icon: const Icon(Icons.add, color: PRIMARY_COLOR),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => DiaryRegisterScreen(),
-                    ),
-                  );
-                })
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 16.0,
+              ),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 12.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) =>
+                                  DiaryDetailScreen(model: data[index])));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: DiaryCard.fromDiaryModel(
+                            model: data[index],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: data.length,
+                ),
+              ),
+            )
           ],
         ),
-        SliverPadding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 16.0,
-          ),
-          sliver: SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 24.0),
-                  child: DiaryCard.fromDiaryModel(
-                    model: DiaryResponseModel(
-                      id: 1,
-                      daysAfterBirth: 10,
-                      writer: "writer",
-                      likeCount: 1,
-                      isDeleted: false,
-                      date: DateTime(2022, 9, 27),
-                      sentences: SAMPLE_SENTENCES,
-                      pictures: SAMPLE_IMAGES,
-                    ),
-                  ),
-                );
-              },
-              childCount: 3,
-            ),
-          ),
-        )
-      ]),
+      ),
     );
   }
 }
