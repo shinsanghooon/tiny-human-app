75f132162647f26fe0e89acda931d43b2fb0238e
sanghoon <shinsanghooon@gmail.com>
2024-01-04 23:51:05 +0900

    feature: 체크리스트 메인 화면 구현

--- lib/checklist/view/checklist_screen.dart
+++ lib/checklist/view/checklist_screen.dart
@@ -1,16 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:tiny_human_app/checklist/model/checklist_detail_model.dart';
+import 'package:tiny_human_app/checklist/model/checklist_model.dart';
+import 'package:tiny_human_app/checklist/view/checklist_detail_screen.dart';
 
+import '../../common/constant/colors.dart';
 import '../../common/layout/default_layout.dart';
+import 'checklist_register_screen.dart';
 
-class CheckListScreen extends StatelessWidget {
+class CheckListScreen extends StatefulWidget {
   const CheckListScreen({super.key});
 
   @override
+  State<CheckListScreen> createState() => _CheckListScreenState();
+}
+
+class _CheckListScreenState extends State<CheckListScreen> {
+  List<ChecklistModel> datas = [
+    ChecklistModel(
+      title: '문화센터 갈 때',
+      checklist: [
+        ChecklistDetailModel(content: '속옷', isChecked: true),
+        ChecklistDetailModel(content: '기저귀', isChecked: false),
+        ChecklistDetailModel(content: '분유', isChecked: false),
+      ],
+    ),
+    ChecklistModel(
+      title: '놀러갈때',
+      checklist: [
+        ChecklistDetailModel(content: '속옷', isChecked: true),
+        ChecklistDetailModel(content: '기저귀', isChecked: false),
+        ChecklistDetailModel(content: '분유', isChecked: false),
+        ChecklistDetailModel(content: '홈캠', isChecked: false),
+      ],
+    ),
+    ChecklistModel(
+      title: '여행',
+      checklist: [
+        ChecklistDetailModel(content: '속옷', isChecked: true),
+        ChecklistDetailModel(content: '기저귀', isChecked: false),
+        ChecklistDetailModel(content: '분유', isChecked: false),
+        ChecklistDetailModel(content: '홈캠', isChecked: false),
+        ChecklistDetailModel(content: '이불', isChecked: false),
+        ChecklistDetailModel(content: '이유식', isChecked: false),
+      ],
+    )
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      child: Center(
-        child: Text("Check List"),
-      ),
-    );
+    return DefaultLayout(
+        appBar: AppBar(
+          title: const Text(
+            "CHECK LIST",
+            style: TextStyle(
+              color: Colors.deepOrange,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.add, color: PRIMARY_COLOR),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => const ChecklistRegisterScreen(),
+                    ),
+                  );
+                })
+          ],
+        ),
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            return Container(
+              height: 80,
+              color: Colors.white,
+              child: InkWell(
+                onTap: () {
+                  print('$index');
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => ChecklistDetailScreen(
+                          checklist: datas[index].checklist)));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 28.0, vertical: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        datas[index].title,
+                        style: const TextStyle(
+                          fontSize: 24.0,
+                        ),
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider(
+              height: 1.0,
+              thickness: 0.8,
+              indent: 16.0,
+              endIndent: 16.0,
+            );
+          },
+          itemCount: datas.length,
+        ));
   }
 }
