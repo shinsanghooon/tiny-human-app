5e0525b2e262785909280ba641918b211169c451
sanghoon <shinsanghooon@gmail.com>
2024-01-29 13:22:48 +0900

    feature: 체크리스트 초기 데이터 로딩 기능 구현

--- lib/checklist/provider/checklist_provider.dart
+++ lib/checklist/provider/checklist_provider.dart
@@ -4,7 +4,7 @@ import '../model/checklist_model.dart';
 import '../repository/checklist_repository.dart';
 
 final checklistProvider =
-StateNotifierProvider<ChecklistNotifier, List<ChecklistModel>>((ref) {
+    StateNotifierProvider<ChecklistNotifier, List<ChecklistModel>>((ref) {
   final repository = ref.watch(checklistRepositoryProvider);
   return ChecklistNotifier(repository: repository);
 });
@@ -15,7 +15,9 @@ class ChecklistNotifier extends StateNotifier<List<ChecklistModel>> {
   ChecklistNotifier({
     required this.repository,
   }) : super([]) {
-
+    // 레포지토리를 써도 결국 id를 통해 User를 조회해야함
+    // accessToken을  서버에 전달하기 때문에 userId를 전달할 필요 없음
+    getMyChecklist();
   }
 
   void addChecklist(ChecklistModel model) {
@@ -24,13 +26,12 @@ class ChecklistNotifier extends StateNotifier<List<ChecklistModel>> {
 
   void updateChecklist(ChecklistModel updatedModel) {
     state = state
-        .map((e) =>
-    e.id == updatedModel.id
-        ? ChecklistModel(
-        id: updatedModel.id,
-        title: updatedModel.title,
-        checklist: updatedModel.checklist)
-        : e)
+        .map((e) => e.id == updatedModel.id
+            ? ChecklistModel(
+                id: updatedModel.id,
+                title: updatedModel.title,
+                checklistDetail: updatedModel.checklistDetail)
+            : e)
         .toList();
   }
 
@@ -39,9 +40,12 @@ class ChecklistNotifier extends StateNotifier<List<ChecklistModel>> {
   }
 
   ChecklistModel getChecklist(int id) {
-    return state
-        .where((e) => e.id == id)
-        .toList()
-        .first;
+    return state.where((e) => e.id == id).toList().first;
+  }
+
+  Future<void> getMyChecklist() async {
+    print('getMyChecklist');
+    final response = await repository.getChecklists();
+    state = response;
   }
 }
