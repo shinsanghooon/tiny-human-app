99b7527cb15c39c89f5e11f15bc317e8f8271b52
sanghoon <shinsanghooon@gmail.com>
2024-03-06 17:27:21 +0900

    feature: 페이지네이션 일반화

---
+++ lib/common/component/pagenation_list_view.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiny_human_app/common/constant/colors.dart';
+
+import '../model/cursor_pagination_model.dart';
+import '../model/model_with_id.dart';
+import '../provider/pagination_provider.dart';
+import '../utils/pagination_utils.dart';
+
+typedef PaginationWidgetBuilder<T extends IModelWithId> = Widget Function(BuildContext context, int index, T model);
+
+class PaginationListView<T extends IModelWithId> extends ConsumerStatefulWidget {
+  final StateNotifierProvider<PaginationProvider, CursorPaginationBase> provider;
+  final PaginationWidgetBuilder<T> itemBuilder;
+
+  const PaginationListView({required this.provider, required this.itemBuilder, super.key});
+
+  @override
+  ConsumerState<PaginationListView> createState() => _PaginationListViewState<T>();
+}
+
+class _PaginationListViewState<T extends IModelWithId> extends ConsumerState<PaginationListView> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(listener);
+  }
+
+  void listener() {
+    PaginationUtils.paginate(
+      controller: controller,
+      provider: ref.read(widget.provider.notifier),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(listener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(widget.provider);
+
+    // 완전 처음 로딩
+    if (state is CursorPaginationLoading) {
+      return const Center(
+        child: CircularProgressIndicator(
+          color: Colors.deepOrange,
+        ),
+      );
+    }
+
+    // 에러
+    if (state is CursorPaginationError) {
+      return Center(
+        child: Text(state.message),
+      );
+    }
+
+    final cp = state as CursorPagination<T>;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: RefreshIndicator(
+        edgeOffset: 100.0,
+        color: PRIMARY_COLOR,
+        onRefresh: () async {
+          ref.read(widget.provider.notifier).paginate(
+                forceRefetch: true,
+              );
+        },
+        child: ListView.separated(
+          physics: const AlwaysScrollableScrollPhysics(),
+          controller: controller,
+          itemBuilder: (_, index) {
+            if (index == cp.body.length) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 16.0,
+                ),
+                child: Center(
+                  child: state is CursorPaginationFetchingMore
+                      ? CircularProgressIndicator(
+                          strokeWidth: 8.0,
+                        )
+                      : Text("마지막 데이터입니다."),
+                ),
+              );
+            }
+
+            final pItem = cp.body[index];
+            return widget.itemBuilder(
+              context,
+              index,
+              pItem,
+            );
+          },
+          separatorBuilder: (_, index) {
+            return const SizedBox(
+              height: 12.0,
+            );
+          },
+          itemCount: cp.body.length + 1,
+        ),
+      ),
+    );
+  }
+}
